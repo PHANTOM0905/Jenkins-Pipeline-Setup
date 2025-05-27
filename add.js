1c6e49932f2441adba5b8e42a48d8ad0adcdfa18
Mohd Saif <saifkaif615@gmail.com>
2025-05-28 01:44:04 +0530

    Add add.js for Jenkins deployment

--- add.js
+++ add.js
@@ -1,4 +1,33 @@
-// add.js
-const a = 5;
-const b = 10;
-console.log(`Sum is: ${a + b}`);
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/' || req.url === '/index.html') {
+        // Serve index.html
+        const filePath = path.join(__dirname, 'index.html');
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.writeHead(500);
+                res.end('Error loading index.html');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+            }
+        });
+    } else {
+        // Handle API request
+        const query = url.parse(req.url, true).query;
+        const a = parseFloat(query.a) || 0;
+        const b = parseFloat(query.b) || 0;
+        const sum = a + b;
+
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ result: sum }));
+    }
+});
+
+server.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
